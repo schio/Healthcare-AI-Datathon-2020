5e1ae76efc59ea0243976e9204d48e089d4e2a76
schio <ghsehr1@gmail.com>
2020-12-11 10:27:59 +0900

    update data_loader

--- EHR/data/data_loader.py
+++ EHR/data/data_loader.py
@@ -1,5 +1,15 @@
+import pandas as pd
+from utils import load_icustays, load_diagnoses, load_lab, split_train_test
 
-from utils import *
 
+feature_list = [load_icustays,
+                load_diagnoses,
+                load_lab
+                ]
 
-load_diagnoses()
+features = []
+for feature in feature_list:
+  features.append(feature())
+df = pd.concat([features], axis=1)
+
+train_x, train_y, test_x, test_y = split_train_test(df)
